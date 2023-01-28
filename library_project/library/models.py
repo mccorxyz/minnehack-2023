c3d5d7778c2339559092e150770b0334b45161a7
Jacob McCormick <jmccormick1858@yahoo.com>
2023-01-28 16:19:54 -0600

    wip: models

--- library_project/library/models.py
+++ library_project/library/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=300)
+    # authors
+    publisher = models.CharField(max_length=100)
+    publishedDate = models.DateField
+    description = models.CharField(max_length=500)
+    isbn = models.CharField(max_length=13)
+    pageCount = models.IntegerField(default=0,
+                                    validators=[
+                                        MinValueValidator(0)
+                                    ])
+    # categories
+    averageRating = models.DecimalField(default=0,
+                                        validators=[
+                                            MinValueValidator(0),
+                                            MaxValueValidator(5)
+                                        ])
+    # maturity
+
+
+
+class BookForm(models.Model):
+    isbn = models.CharField(max_length=13)
--- library_project/library/views.py
+++ library_project/library/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from isbnlookup.isbnlookup import ISBNLookup
+from django.contrib import messages
 
 # Create your views here.
 def library(request):
     return render(request, "library/library.html")
 
 def new_book(request):
+
+    if request.method == "POST":
+        bookForm = BookForm(request.POST)
+        if bookForm.is_valid():
+            ISBNLookup().lookup(bookForm.isbn)
+            messages.info(request, "Adding book!")
+            bookForm.save()
+            return redirect("library")
     return render(request, "library/new-book.html")
 
 def check_in(request):
