0b945a3ca0a8da04c8e8d297467ebda0fa484a5f
knuts983 <knuts983@umn.edu>
2023-01-29 09:46:48 -0600

    non-interactive

--- start-container.sh
+++ start-container.sh
@@ -2,7 +2,7 @@
 #  -v /etc/letsencrypt/live/cknutson.org-0001/cert.pem:/app/cert.pem:ro \
 #  -v /etc/letsencrypt/live/cknutson.org-0001/privkey.pem:/app/privkey.pem:ro \
 
-docker run -it --name=bookend --privileged \
+docker run -d --name=bookend --privileged \
   -v "${PWD}"/library_project/db.sqlite3:/app/db.sqlite3:rw \
   --rm -p 80:80 \
-  rogueraptor7/bookend:latest /bin/bash
+  rogueraptor7/bookend:latest
