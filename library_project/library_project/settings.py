256c2fce69b4d4ff1e946dcd875415305e57fafe
knuts983 <knuts983@umn.edu>
2023-01-29 09:05:34 -0600

    SSL kinda working

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tze1moomr73kvhsj8j*9ewnet8izs#z+@_5lv9za3e$pdkowyt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["cknutson.org", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["198.46.160.189", "cknutson.org", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'api',
     'website',
     'library',
-    'django_tables2'
+    'django_tables2',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- library_project/run-server.sh
+++ library_project/run-server.sh
@@ -2,4 +2,5 @@
 
 #python3 manage.py migrate
 #python3 manage.py makemigrations
-python3 manage.py runserver 0.0.0.0:80
+#python3 manage.py runserver 0.0.0.0:80
+python3 manage.py runserver_plus 0.0.0.0:443 --cert-file /etc/l#python3 manage.py runserver_plus 0.0.0.0:443etsencrypt/live/cknutson.org/cert.pem --key-file /etc/letsencrypt/live/cknutson.org/key.pem
