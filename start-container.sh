9aa33ba2f0e7fdb83be3e714fce021322acb5b3d
knuts983 <knuts983@umn.edu>
2023-01-29 09:44:54 -0600

    no ssl

--- start-container.sh
+++ start-container.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#  -v /etc/letsencrypt/live/cknutson.org-0001/cert.pem:/app/cert.pem:ro \
+#  -v /etc/letsencrypt/live/cknutson.org-0001/privkey.pem:/app/privkey.pem:ro \
 
-docker run -d --name=bookend --privileged -v "${PWD}"/library_project/db.sqlite3:/app/db.sqlite3:rw --rm -p 80:80 rogueraptor7/bookend:latest
+docker run -it --name=bookend --privileged \
+  -v "${PWD}"/library_project/db.sqlite3:/app/db.sqlite3:rw \
+  --rm -p 80:80 \
+  rogueraptor7/bookend:latest /bin/bash
