6dfba86cdef964dd7fa089a1faeef8bbcc44d5b9
Jacob McCormick <jmccormick1858@yahoo.com>
2023-01-29 01:07:38 -0600

    more table changes

--- library_project/library/tables.py
+++ library_project/library/tables.py
@@ -2,6 +2,7 @@ import django_tables2 as tables
 from .models import Book
 
 class BookTable(tables.Table):
-    title = tables.Column()
     class Meta:
         model = Book
+        attrs = {"class": "table",}
+        fields = ("id", "title", "authors", "quantity")
